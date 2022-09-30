8ae3b53b2414489f0710b254eb2db3fc70c88cf6
OlegStankevich1973 <stankeviz@mail.ru>
2022-09-30 22:58:41 +0300

    Программа превращения трехзначного числа в двухзначное

--- task17.py
+++ task17.py
@@ -1,16 +1,20 @@
 #	Удалить вторую цифру трёхзначного числа.
-# number = int(input('Ввведите данные '))
-# res = (((number // 100)*10) + (number % 10)) 
+number = int(input('Ввведите данные '))
+res = (((number // 100)*10) + (number % 10)) 
 
 # или так чисел
 
-# n1 = number // 100
-# n2 = number % 10
-# # res = n1 * 10 + n2
-# print(res)
+n1 = number // 100
+n2 = number % 10
+res = n1 * 10 + n2
+print(res)
 
 # или так для строк
 number = input('Ввведите данные ')
-print(number[0] + number[2])
+print(number[0] + number[2])# складываем элементы строки
 
+# или еще так для списков
+number = list(str(123))
+del number[1]
+print(''.join(number)) #склеивает элементы списка
 
