1b17f22aa6487d5aa1e8686118db00fda0168bcf
OlegStankevich1973 <stankeviz@mail.ru>
2022-10-11 21:54:14 +0300

    Программа складывает между собой многочлены квадратных уравнений и записывает их в файлы

--- task42DZ4.py
+++ task42DZ4.py
@@ -38,25 +38,37 @@ def polinome(k, file_name):
     # формирует список коэфициентов случайное число из диапазона 0,k +1
     factors = [randint(1, 101) for _ in range(0, k + 1)]
     res = create_formula(factors)
-
+# создаем и открываем и записываем файл для записи формулы многочлена
     with open(file_name, "w", encoding="utf-8") as f:
+        # из значений стр 39 в файле в обратную сторону проходим эти цифры и записываем их в файл
         f.write(' '.join([str(i) for i in factors[::-1]]) + '\n')
+# получаем формулу
         f.write(res)
 
 
-polinome(3, 'file1.txt')
+# запускаем метод polinome(максимальная степень многочлена,файл откуда и куда ложатся данные)
+polinome(4, 'file1.txt')
+
+# создаем второе уравнение и записываем его в файл 2
 polinome(3, 'file2.txt')
-pol_1 = []
+# открываем файлы и проходим по их данным
 with open('file1.txt')as f1, open('file2.txt') as f2:
     factors1 = [int(i) for i in f1.readline().split()]
     factors2 = [int(i) for i in f2.readline().split()]
-if len(factors1) > len(factors2):
+    #сравниваем количество переменных в каждом многочлене 
+if len(factors1) == len(factors2):
+    # испльзуем функцию zip для сложения одинакового количества членов
     res = [a + b for a, b in zip(factors1, factors2)]
+    # при разном количестве многочленов  в двух уравнениях
 elif len(factors1) > len(factors2):
+    # копируем недостающие переменные из большего (1 )уравнения
     res = factors1.copy()
+    # прооходим все переменные второго уравнения
     for i in range(len(factors2)):
+        # добавляем к копии недостающие элементы
         res[i] += factors2[i]
 else:
+    # тоже самое если 2 уравнение больше
     res = factors2.copy()
     for i in range(len(factors1)):
         res[i] += factors1[i]
